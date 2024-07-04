0e169ce08c9bba6cfed693569eae0f406d552469
S-MouadMeziani <mouad@schnaq.com>
2024-07-04 11:21:12 +0200

    Pre populated Code with some small changes

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <p>Dashboard V2 Page</p>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import "@/app/ui/global.css"
+import { inter } from "@/app/ui/fonts";
+import {Metadata} from "next";
+export const metaData: Metadata = {
+
+  title: "Acme Layout"
+}
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className = { `${inter.className} antialiased` } >{children}</body>
     </html>
   );
 }
